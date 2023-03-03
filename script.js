5281c91f4c11f3625d1676e22c9a8e47d1ba5f9b
Mariano Tourné <marianotourne@gmail.com>
2023-03-03 19:53:08 -0300

    informe almost ready

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const navbar = document.getElementById("navbar");
 const formulario = document.getElementById("formulario");
 const informe = document.getElementById("informe");
 const informeNumeracion = document.getElementById("informeNumeracion");
+const informeResultados = document.getElementById("informeResultados");
 const datosRemitente = document.getElementById("datosRemitente");
 const numeroLaboratorio = document.getElementById("numeroLaboratorio");
 const numeroPropio = document.getElementById("numeroPropio");
@@ -38,13 +39,13 @@ const pseudomonaOption = Array.apply(
 );
 
 agua.onclick = toggleOptions;
-btnEnviar.onclick = enviarResultados;
+btnEnviar.onclick = todos;
 
 function toggleOptions() {
   formulario.classList.toggle("hidden");
 }
 
-function enviarResultados() {
+function enviarEncabezado() {
   nroLab = numeroLaboratorio.valueAsNumber;
   nroPropio = numeroPropio.valueAsNumber;
   nomRemitente = nombreRemitente.value;
@@ -52,12 +53,7 @@ function enviarResultados() {
   recepcion = fechaRecepcion.value;
   inicio = fechaInicio.value;
   detalle = detalleMuestra.value;
-  let recuentos = getRecuentosOption();
-  let bacteriasColiformes = getBacteriasColiformesOption();
-  let coliformesTermotolerantes = getColiformesTermotolerantesOption();
-  let escherichiaColi = getEscherichiaOption();
-  let pseudomonaOption = getPseudomonaOption();
-  imprimirResultado(
+  imprimirEncabezado(
     nroLab,
     nroPropio,
     nomRemitente,
@@ -66,14 +62,56 @@ function enviarResultados() {
     inicio,
     detalle
   );
-  // console.log(`Recuentos es: ${recuentos}`);
-  // console.log(`Bacterias Coliformes es: ${bacteriasColiformes}`);
-  // console.log(`Coliformes Termotolerantes es: ${coliformesTermotolerantes}`);
-  // console.log(`Escherichia Coli es: ${escherichiaColi}`);
-  // console.log(`Pseudomona es: ${pseudomonaOption}`);
+}
+function enviarResultados() {
+  let recuentos = getRecuentosOption();
+  let bacteriasColiformes = getBacteriasColiformesOption();
+  let coliformesTermotolerantes = getColiformesTermotolerantesOption();
+  let escherichiaColi = getEscherichiaOption();
+  let pseudomona = getPseudomonaOption();
+
+  let leyendaRecuentos = "";
+  if (recuentos == ">500") {
+    leyendaRecuentos = "Calidad Deficiente";
+  } else if (recuentos == "<10") {
+    leyendaRecuentos = "Calidad Aceptable";
+  }
+
+  let leyendaBacteriasColiformes = "Presencia";
+  if (bacteriasColiformes == "<1.1") {
+    leyendaBacteriasColiformes = "Ausencia / No se detecta";
+  }
+
+  let leyendaColiformesTermotolerantes = "Presencia";
+  if (coliformesTermotolerantes == "<1.1") {
+    leyendaColiformesTermotolerantes = "Ausencia / No se detecta";
+  }
+
+  let leyendaEscherichiaColi = "Presencia";
+  if (escherichiaColi == "Ausencia") {
+    leyendaEscherichiaColi = "Ausencia / No se detecta";
+  }
+
+  let leyendaPseudomona = "Presencia";
+  if (pseudomona == "Ausencia") {
+    leyendaPseudomona = "Ausencia / No se detecta";
+  }
+
+  imprimirResultados(
+    recuentos,
+    bacteriasColiformes,
+    coliformesTermotolerantes,
+    escherichiaColi,
+    pseudomona,
+    leyendaRecuentos,
+    leyendaBacteriasColiformes,
+    leyendaColiformesTermotolerantes,
+    leyendaEscherichiaColi,
+    leyendaPseudomona
+  );
 }
 
-function imprimirResultado(
+function imprimirEncabezado(
   nroLab,
   nroPropio,
   nombre,
@@ -86,22 +124,81 @@ function imprimirResultado(
     <p>Nº ${nroLab}-${nroPropio}</p>
     `;
   datosRemitente.innerHTML = `
-    <p class="column1">Remite la muestra<p>
-    <p class="column2">${nombre}</p>
-    <p class="column1">Direccion<p>
-    <p class="column2">${direccion}</p>
-    <p class="column1">Recepción muestra<p>
-    <p class="column2">${recepcion}</p>
-    <p class="column1">Fecha de inicio<p>
-    <p class="column2">${inicio}</p>
-    <p class="column1">Detalle de muestra<p>
-    <p class="column2">${detalle}</p>
+    <div>
+      <p>Remite la muestra</p>
+      <p>${nombre}</p>
+    </div>
+    <div>
+      <p>Direccion</p>
+      <p>${direccion}</p>
+    </div>
+    <div>
+      <p>Recepción muestra</p>
+      <p>${recepcion}</p>
+    </div>
+    <div>
+      <p>Fecha de inicio</p>
+      <p>${inicio}</p>
+    </div>
+    <div>
+      <p>Detalle de muestra</p>
+      <p>${detalle}</p>
+    </div>
   `;
-  // navbar.classList.toggle("hidden");
-  // formulario.classList.toggle("hidden");
-  // informe.classList.toggle("hidden");
+  navbar.classList.toggle("hidden");
+  formulario.classList.toggle("hidden");
+  informe.classList.toggle("hidden");
 }
 
+function imprimirResultados(
+  recuentos,
+  bacteriasColiformes,
+  coliformesTermotolerantes,
+  escherichiaColi,
+  pseudomona,
+  leyendaRecuentos,
+  leyendaBacteriasColiformes,
+  leyendaColiformesTermotolerantes,
+  leyendaEscherichiaColi,
+  leyendaPseudomona
+) {
+  informeResultados.innerHTML = `
+    <div>
+      <p>Recuento de aerobias mesófilas</p>
+      <p>${leyendaRecuentos}</p>
+      <p>${recuentos} UFC/ml</p>
+    </div>
+    <div>
+      <p>NPM Bacterias coliformes</p>
+      <p>${leyendaBacteriasColiformes}</p>
+      <p>${bacteriasColiformes} NMP/100ml</p>
+    </div>
+    <div>
+      <p>NPM Coliformes termotolerantes</p>
+      <p>${leyendaColiformesTermotolerantes}</p>
+      <p>${coliformesTermotolerantes} NMP/100ml</p>
+    </div>
+    <div>
+      <p class="cursiva">Escherichia Coli</p>
+      <p>${leyendaEscherichiaColi}</p>
+      <p>${escherichiaColi}/100ml</p>
+    </div>
+    <div>
+      <p class="cursiva">Pseudomona aeruginosa</p>
+      <p>${leyendaPseudomona}</p>
+      <p>${pseudomona}/100ml</p>
+    </div>
+  `;
+}
+
+function getRecuentosOption() {
+  if (recuentosOption.find((item) => item.checked) === undefined) {
+    itemSelected = option11.value;
+  } else {
+    itemSelected = recuentosOption.find((item) => item.checked).value;
+  }
+  return itemSelected;
+}
 function getRecuentosOption() {
   if (recuentosOption.find((item) => item.checked) === undefined) {
     itemSelected = option11.value;
@@ -129,4 +226,9 @@ function getEscherichiaOption() {
 function getPseudomonaOption() {
   let itemSelected = pseudomonaOption.find((item) => item.checked);
   return itemSelected.value;
+}
+
+function todos() {
+  enviarEncabezado();
+  enviarResultados();
 }
